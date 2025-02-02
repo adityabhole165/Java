6bae09351dcabbf3290570cb6c411f5e456df888
adityabhole165 <adityabhole0304@gmail.com>
2025-02-02 13:06:06 +0530

    new folder Created and managed

---
+++ javaBasics/JavaBasics.java
@@ -0,0 +1,49 @@
+package javaBasics;
+public class JavaBasics { //in java class name and file name must same
+    public static void main(String args []) { //its a function , it is a main function , Compiler first find this code first and executes it 
+
+    
+        //Output Statement
+        // System.out.print("Hello World!");
+    
+       System.out.print("Hello Aditya !");
+        System.out.print("Hello Aditya !");
+        System.out.print("Hello Aditya !");
+ 
+    
+    
+
+        //System.out.print();  Statement print data in same line I wanted it to go to the next line 
+
+        System.out.println(" Hei Aditya !");
+        System.out.println("Hei Aditya !");
+        System.out.println("Hei Aditya !");
+    
+
+    
+
+        //Also we can use \n fo going to the next line
+        System.out.print(" Hei Aditya ! \n");
+        System.out.print("Hei Aditya ! \n");
+        System.out.print("Hei Aditya ! \n");
+        System.out.println(" Hei Aditya ! hi \n Aditya");
+
+        
+    
+
+    
+        // Printing star pattern using Println
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+        
+
+
+        
+        
+    }
+
+}
+
+// this whole code is Bolierplate code 
